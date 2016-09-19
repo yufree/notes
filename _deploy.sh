0dc48774baaf64b04dac6683f9ebcde75845056c
Yufree <yufree@live.cn>
2016-09-18 20:56:59 -0400

    add dir

--- _deploy.sh
+++ _deploy.sh
@@ -8,8 +8,9 @@ set -e
 git config --global user.email "yufree@live.cn"
 git config --global user.name "yufree"
 
-git clone -b gh-pages https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git _site
-cd _site
+git clone -b gh-pages https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git notes
+cd notes
+cp -r ../_site/* ./
 git add --all *
 git commit -m "Update the notes" || true
 git push origin gh-pages
