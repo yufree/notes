b03303a9b0616c890f4a77f47c7f97ae54f71755
Yufree <yufree@live.cn>
2016-09-20 14:45:24 -0400

    update deploy

--- _deploy.sh
+++ _deploy.sh
@@ -2,9 +2,6 @@
 
 set -e
 
-[ -z "${GITHUB_PAT}" ] && exit 0
-[ "${TRAVIS_BRANCH}" != "master" ] && exit 0
-
 git config --global user.email "yufree@live.cn"
 git config --global user.name "yufree"
 
