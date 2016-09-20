fb11e4fd3404faa8707896c99aa5c9e4191d9762
Yufree <yufree@live.cn>
2016-09-20 10:50:34 -0400

    add

--- _deploy.sh
+++ _deploy.sh
@@ -11,6 +11,6 @@ git config --global user.name "yufree"
 git clone -b gh-pages https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git notes_site
 cd notes_site
 cp -r ../_book/* ./
-git add --all *
+git add .
 git commit -m "Update the book" || true
 git push origin gh-pages
