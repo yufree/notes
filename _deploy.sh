e8d8742c381a0fd221ccef81f8f9314e09391666
Yufree <yufree@live.cn>
2016-09-20 09:47:10 -0400

    change name

--- _deploy.sh
+++ _deploy.sh
@@ -8,8 +8,8 @@ set -e
 git config --global user.email "yufree@live.cn"
 git config --global user.name "yufree"
 
-git clone -b gh-pages https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git notes
-cd notes
+git clone -b gh-pages https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git notes_site
+cd notes_site
 cp -r ../_book/* ./
 git add --all *
 git commit -m "Update the book" || true
